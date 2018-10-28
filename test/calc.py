d70c5271540b2eedf77396ba61c5cef63bcf0592
vtheno <a2550591@gmail.com>
2018-10-28 15:49:23 +0800

    add readme & calc

--- test/calc.py
+++ test/calc.py
@@ -5,7 +5,11 @@ from lr1p.lex import Lexical
 skips = [" ","\n","\t"]
 lex = Lexical(skips)
 def sym2sym(s):
-    if s == "add":
+    if s == "lparent":
+        return "("
+    elif s == "rparent":
+        return ")"
+    elif s == "add":
         return "+"
     elif s == "sub":
         return "-"
@@ -23,12 +27,60 @@ parse = parser(sym2sym,"ident")
 def program(expr:Vn) -> Vn:
     return {'program':expr}
 @parse.add_rule
-def expr(ident:Vt) -> Vn:
+def expr(term:Vn,add:Vt,expr:Vn)->Vn:
+    """
+    expr -> term + expr
+    """
+    return {"add":[term,expr]}
+@parse.add_rule
+def expr(term:Vn,sub:Vt,expr:Vn)->Vn:
+    """
+    expr -> term - expr
+    """
+    return {"sub":[term,expr]}
+@parse.add_rule
+def expr(term:Vn)->Vn:
+    """
+    expr -> term
+    """
+    return term
+@parse.add_rule
+def term(factor:Vn,mul:Vt,term:Vn)->Vn:
+    """
+    term -> factor * term
+    """
+    return {"mul":[factor,term]}
+@parse.add_rule
+def term(factor:Vn,div:Vt,term:Vn)->Vn:
+    """
+    term -> factor / term
+    """
+    return {"div":[factor,term]}
+@parse.add_rule
+def term(factor:Vn)->Vn:
+    """
+    temr -> factor
+    """
+    return factor
+@parse.add_rule
+def factor(ident:Vt) -> Vn:
+    """
+    factor -> ident
+    ident -> num
+    ident -> var
+    """
     if ident.isdigit():
         return {"num":int(ident)}
     return {"var":ident}
 @parse.add_rule
-def expr(expr_1:Vn,add:Vt,expr_2:Vn)->Vn:
-    return {"add":[expr_1,expr_2]}
-
-parse = parse.build()
+def factor(lparent:Vt,expr:Vn,rparent:Vt)->Vn:
+    """
+    factor -> ( expr )
+    """
+    return expr
+parse = parse.build(lex)
+print(parse("2 * (1 + 3) / 2"))
+print(parse("2 * 1 + 3 / 2"))
+# output: 
+# {'program': {'mul': [{'num': 2}, {'div': [{'add': [{'num': 1}, {'num': 3}]}, {'num': 2}]}]}}
+# {'program': {'add': [{'mul': [{'num': 2}, {'num': 1}]}, {'div': [{'num': 3}, {'num': 2}]}]}}
